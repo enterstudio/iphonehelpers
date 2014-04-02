9b32796d2de5baf1289ed65e34bfc9b021ae4219
miketihonchik <mike.tihonchik@gmail.com>
2014-04-02 12:00:08 -0500

    complete demo

--- CardIODemo/CardIODemo/Helpers.h
+++ CardIODemo/CardIODemo/Helpers.h
@@ -1,17 +1,20 @@
 //
-//  Constants.h
+//  Helpers.h
 //  CardIODemo
 //
-//  Created by mike.tihonchik on 4/1/14.
+//  Created by mike.tihonchik on 4/2/14.
 //  Copyright (c) 2014 mike.tihonchik. All rights reserved.
 //
 
-#ifndef CardIODemo_Constants_h
-#define CardIODemo_Constants_h
+#ifndef CardIODemo_Helpers_h
+#define CardIODemo_Helpers_h
 
 #define HELVETICA_BOLD(x) [UIFont fontWithName:@"HelveticaNeue-Bold" size:x]
 #define EFFRA_MEDIUM(x) [UIFont fontWithName:@"Effra-Medium" size:x]
+#define EFFRA_LIGHT(x) [UIFont fontWithName:@"Effra-Light" size:x]
 
 #define IS_IPHONE_5 ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
+
+#define HEXCOLOR(c) [UIColor colorWithRed:((c>>24)&0xFF)/255.0f green:((c>>16)&0xFF)/255.0f blue:((c>>8)&0xFF)/255.0f alpha:((c)&0xFF)/255.0f];
 
 #endif
--- CardIODemo/CardIODemo/ViewController.m
+++ CardIODemo/CardIODemo/ViewController.m
@@ -8,11 +8,17 @@
 
 #import "ViewController.h"
 #import "CardIO.h"
-#import "Constants.h"
+#import "Helpers.h"
 
 @interface ViewController () <CardIOPaymentViewControllerDelegate>
 
-@property (weak, nonatomic) IBOutlet UILabel *infoLabel;
+@property (retain, nonatomic) UIButton *addNewCCBtn;
+@property (retain, nonatomic) UILabel *ccNumberLabel;
+@property (retain, nonatomic) UILabel *ccNumber;
+@property (retain, nonatomic) UILabel *expirationLabel;
+@property (retain, nonatomic) UILabel *expiration;
+@property (retain, nonatomic) UILabel *cvvLabel;
+@property (retain, nonatomic) UILabel *cvv;
 
 @end
 
@@ -23,9 +29,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.infoLabel.text = @"";
 
     [self setupGuiStyle];
+    [self setupLabels];
 }
 
 #pragma mark - User Actions
@@ -44,7 +50,9 @@
     // Do whatever needs to be done to deliver the purchased items.
     [self dismissViewControllerAnimated:YES completion:nil];
     
-    self.infoLabel.text = [NSString stringWithFormat:@"Received card info. Number: %@, expiry: %02lu/%lu, cvv: %@.", info.redactedCardNumber, (unsigned long)info.expiryMonth, (unsigned long)info.expiryYear, info.cvv];
+    self.ccNumber.text = info.redactedCardNumber;
+    self.expiration.text = [NSString stringWithFormat:@"%02lu/%lu", (unsigned long)info.expiryMonth, (unsigned long)info.expiryYear];
+    self.cvv.text = info.cvv;
 }
 
 - (void)userDidCancelPaymentViewController:(CardIOPaymentViewController *)paymentViewController {
@@ -65,6 +73,15 @@
     UIImageView *backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:backgroundImage]];
     backgroundView.frame = self.view.bounds;
     [self.view addSubview:backgroundView];
+    
+    _addNewCCBtn = [[UIButton alloc] init];
+    [_addNewCCBtn setTitle:@"SCAN CREDIT CARD" forState:UIControlStateNormal];
+    [_addNewCCBtn.titleLabel setFont:EFFRA_MEDIUM(20)];
+    [_addNewCCBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [_addNewCCBtn setBackgroundImage:[UIImage imageNamed:@"primButton.png"] forState:UIControlStateNormal];
+    _addNewCCBtn.frame = CGRectMake(20.0, 60, 280, 40);
+    [_addNewCCBtn addTarget:self action:@selector(scanCardClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_addNewCCBtn];
 
     UIImageView *logo = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"cardio.png"]];
     logo.frame = CGRectMake(5, screenHeight - 220, logo.frame.size.width, logo.frame.size.height);
@@ -78,6 +95,54 @@
     demoView.frame = CGRectMake(200.0, screenHeight - 110, 100.0, 100.0);;
     [self.view addSubview:demoView];
     
+}
+
+- (void)setupLabels {
+    _ccNumberLabel = [[UILabel alloc] init];
+    _ccNumberLabel.text = @"Credit Card #:";
+    [_ccNumberLabel setFont:EFFRA_LIGHT(16)];
+    [_ccNumberLabel setTextColor:[self darkGrayColor]];
+    _ccNumberLabel.frame = CGRectMake(20.0, 110, 160, 20);
+    [self.view addSubview:_ccNumberLabel];
+
+    _ccNumber = [[UILabel alloc] init];
+    _ccNumber.text = @"";
+    [_ccNumber setFont:EFFRA_LIGHT(14)];
+    [_ccNumber setTextColor:[UIColor blackColor]];
+    _ccNumber.frame = CGRectMake(40.0, 130, 200, 20);
+    [self.view addSubview:_ccNumber];
+
+    _expirationLabel = [[UILabel alloc] init];
+    _expirationLabel.text = @"Expiration:";
+    [_expirationLabel setFont:EFFRA_LIGHT(16)];
+    [_expirationLabel setTextColor:[self darkGrayColor]];
+    _expirationLabel.frame = CGRectMake(20.0, 150, 160, 20);
+    [self.view addSubview:_expirationLabel];
+
+    _expiration = [[UILabel alloc] init];
+    _expiration.text = @"";
+    [_expiration setFont:EFFRA_LIGHT(14)];
+    [_expiration setTextColor:[UIColor blackColor]];
+    _expiration.frame = CGRectMake(40.0, 170, 160, 20);
+    [self.view addSubview:_expiration];
+
+    _cvvLabel = [[UILabel alloc] init];
+    _cvvLabel.text = @"CVV #:";
+    [_cvvLabel setFont:EFFRA_LIGHT(16)];
+    [_cvvLabel setTextColor:[self darkGrayColor]];
+    _cvvLabel.frame = CGRectMake(20.0, 190, 160, 20);
+    [self.view addSubview:_cvvLabel];
+
+    _cvv = [[UILabel alloc] init];
+    _cvv.text = @"";
+    [_cvv setFont:EFFRA_LIGHT(14)];
+    [_cvv setTextColor:[UIColor blackColor]];
+    _cvv.frame = CGRectMake(40.0, 210, 160, 20);
+    [self.view addSubview:_cvv];
+}
+
+-(UIColor *)darkGrayColor {
+    return HEXCOLOR(0x3F3F40FF);
 }
 
 @end
